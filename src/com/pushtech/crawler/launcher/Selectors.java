5b68cb3867c2cf2939eb8ef88d6cb341e8f7b765
fampilahy <fampilahy@valueit.fr>
2016-08-04 12:51:21 +0300

    FIRST

---
+++ src/com/pushtech/crawler/launcher/Selectors.java
@@ -0,0 +1,34 @@
+package com.pushtech.crawler.launcher;
+
+public class Selectors {
+
+   // product page
+   public static final String PRODUCT_PAGE_IDENTIFIER = "#productSidebar";
+   public static final String PRODUCT_NAME = ".productName.noBg>span";
+   public static final String PRODUCT_LINK = "head > link";
+   public static final String PRODUCT_DESCRIPTION = "meta[name=Description]";
+   public static final String PRODUCT_BRAND = "";
+   public static final String PRODUCT_KEYWORDS = "meta[name=keywords]";
+   public static final String PRODUCT_IDENTIFIER = ".details > div:contains(eference)";
+   public static final String PRODUCT_CATEGORY = "crumb0>a";// listing
+   public static final String PRODUCT_IMAGE = ".slide > img";
+   public static final String PRODUCT_PRICE = "#articlePrice.price >span > span#price";
+   public static final String PRODUCT_DECIMAL_PRICE = "#price_decimals";
+
+   public static final String PRODUCT_SHIPPING_COST = ".inlineBlock:contains(Livraison)";
+   public static final String PRODUCT_SHIPPING_DELAY = ".availability.inctdclks>p>strong:not([itemprop])";
+
+   public static final String PRODUCT_QUANTITY = ".max_stock";
+   public static final String PRODUCT_DELIVERY = "div.delay-livraison>div:contains(Livraison)";
+
+   // listing page
+   public static final String LISTING_PAGE_IDENTIFIER = "ul#productsList>li";
+   public static final String LISTING_PAGE_PRODUCTS = "ul#productsList>li";
+   public static final String LISTING_PAGE_PRODUCT_LINK = "div.productBox>a";
+   public static final String NEXT_PAGE_LINK = "a.next_page";
+
+   // home page
+   public static final String ALL_LISTING = ".control-group > a"; //
+   public static final String HOME_PAGE_IDENTIFIER = ".homepage_title";
+
+}
